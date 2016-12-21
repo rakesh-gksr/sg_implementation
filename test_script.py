76cd74dc9a1d5884da90fbb3d2ac980702205eff
rakesh-gksr <rakesh.gksr@gmail.com>
2016-12-21 16:54:15 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -14,16 +14,6 @@ def get_driver():
         command_executor="http://192.168.201.164:4445/wd/hub",
         desired_capabilities=capabilities)
 	driver.maximize_window()
-	import requests
-	vnc_url = "{hub_url}grid/api/testsession?session={session_id}".format(
-        hub_url=driver.command_executor._url[:-6],
-        session_id=driver.session_id
-        )
-	req = requests.post(vnc_url)
-	node_info = req.json()
-	local_ip = node_info['proxyId']
-	vnc_address =  "vnc" + local_ip[4:] + "1"
-	print ('VNC address is {}').format(vnc_address.split('vnc://')[1])
 	return driver
 
 def test_one():
